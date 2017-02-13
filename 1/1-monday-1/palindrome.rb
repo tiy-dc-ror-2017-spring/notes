571a6406155a7815841d016cebf49b6465302005
Russell Osborne <russell@burningpony.com>
2017-02-13 12:16:29 -0500

    Completed palindrome in class

--- 1/1-monday-1/palindrome.rb
+++ 1/1-monday-1/palindrome.rb
@@ -1,11 +1,22 @@
 # get a word from a user
-p 1
+# answer = "tacocat"
+number_of_times_to_run = 5
+number_of_questions_asked = 0
 
-print "1"
-print "1"
-puts "1"
+while number_of_questions_asked <= number_of_times_to_run
+  puts "asking for the " + number_of_questions_asked + " time"
+  puts "Whats the word?"
+  answer = gets.chomp
 
-puts "I will show up!"
-# reverse and check if its equal
-#
-# tel the user?
+  # p answer
+
+  # reverse and check if its equal
+  if answer.reverse == answer && answer.length > 1
+    puts "The word is indeed a palindrome watson!"
+  else
+    puts "You Fool Watson! " + answer.reverse + " is not a palindrome"
+  end
+
+  number_of_questions_asked = number_of_questions_asked + 1
+  # number_of_questions_asked += 1
+end
---
+++ 1/1-monday-1/pry_history.rb
@@ -0,0 +1,35 @@
+1+1
+9 + 8 
+1.1 * 2
+exit
+18446744073709551615
+1/3
+1./(3)
+1./(3.0)
+1.to_f
+1.to_s
+1.to_d
+1.to_c
+1.to_r
+ls 1.to_c
+(1.to_f).to_s
+(1.to_f).to_s.to_i
+exit
+"1" == "1"
+"1" == "12"
+"1" == 1
+exit
+1 + 1 
+"1" + "1"
+"1 " + "1"
+word = "stressed"
+"1" + word + "1"
+"1 " + word + " 1"
+exit
+"123".length
+exit
+" 1 " + 1
+answer
+"1".reverser
+;0aw948u58dih[fads
+exit
