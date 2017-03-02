ecda25be0630363e9f33b9952a87e84f65f8f0d4
Russell Osborne <russell@theironyard.com>
2017-03-02 15:32:40 -0500

    Adding the HW review NO CHEATING

---
+++ 3/4-thursday-12/employeee_reviews_solution/department.rb
@@ -0,0 +1,25 @@
+require './employee'
+require 'pry'
+
+class Department < ActiveRecord::Base
+
+  def staff
+    Employee.where(department_id: self.id)
+  end
+
+  def add_employee(new_employee)
+    new_employee.department_id = self.id
+    new_employee.save
+  end
+
+  def department_salary
+    self.staff.reduce(0.0) {|sum, e| sum + e.salary}
+  end
+
+  def department_raise(alloted_amount)
+    raise_eligible = staff.select {|e| yield(e)}
+
+    amount = alloted_amount / raise_eligible.length
+    raise_eligible.each {|e| e.raise_by_amount(amount)}
+  end
+end
---
+++ 3/4-thursday-12/employeee_reviews_solution/pry_history.rb
@@ -0,0 +1,47 @@
+new_employee
+self
+exit
+self
+new_employee
+self
+new_employee
+new_employee.department_id = id
+id
+name
+new_employee.department_id = self.id
+exit
+Employee.find
+Employee.find 1
+Employee.all
+Employee.create(name: "Russell", email: "r@email.com")
+Employee.all
+Employee
+Employee.where(department_id: 1)
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+Employee.where(department_id: 1)
+exit
+xavier
+xavier.id
+Employee.new name: "Matt"
+matt = _
+matt.save
+matt
+updated_xavier = Employee.find(xavier.id)
+xavier.id
+updated_xavier
+xavier
+exit
+raise_eligible
+exit
+raise_eligible
+staff
+exit
+staff
+raise_eligible
+exit
+whereami
+raise_eligible
+raise_eligible.first
+raise_eligible.first.persisted?
+raise_eligible.first.changes
+exit
