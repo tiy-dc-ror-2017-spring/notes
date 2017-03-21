d73a5b66545a4bf1f65a0031464313c26371e654
Russell Osborne <russell@theironyard.com>
2017-03-21 12:17:15 -0400

    Adding bookings controller CR of CRUD

--- 6/2-tuesday-22/tour/config/routes.rb
+++ 6/2-tuesday-22/tour/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  # get 'bookings/index', controller: "bookings", action: "index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
   get "/", controller: "welcome", action: "homepage"
+
+  get "/bookings", controller: "bookings", action: "index"
+  get "/bookings/new", controller: "bookings", action: "new"
+
+  post "/bookings", controller: "bookings", action: "create"
 end
